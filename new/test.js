ed864c72f3d524eef6a6578140e4559bf67c88ce
motivate123 <151899380+motivate123@users.noreply.github.com>
2023-11-24 05:29:17 +0000

    okdone

--- new/test.js
+++ new/test.js
@@ -1,1 +1,10 @@
-console.log("hitesh");
+const accountid=123
+let city="mumbai88";
+
+var new2="iui78u";
+let state;
+console.table([accountid,city,new2,state]);
+/*
+prefer not to use var
+beacuse of its block scope and functional scope
+*/
