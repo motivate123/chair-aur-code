5b343c4bc85258d3901bdfd1bbdddec959217b84
motivate123 <151899380+motivate123@users.noreply.github.com>
2023-11-27 05:13:14 +0000

    datatypes

---
+++ new/datatypes.js
@@ -0,0 +1,13 @@
+//data types
+let name="neeraj"//string datatype
+let islogedin=false //boolean
+let age=18 //number datatype
+//number -2 to power 53
+//bigint
+//string -""
+//null--stand alone value
+//undefined
+//symbol--unique
+console.log(typeof age)//number
+console.log(typeof undefined)//undefined
+console.log(typeof null)// object , dont be confused now learn after now
