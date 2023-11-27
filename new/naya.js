58433f8d0e75059fcff9bc84616de9943e620c86
motivate123 <151899380+motivate123@users.noreply.github.com>
2023-11-27 04:52:03 +0000

    done

---
+++ new/naya.js
@@ -0,0 +1,6 @@
+"use strict"; // treat all code of js as newer version
+
+//alert(3+3)// we r using nodejs , not browser , in browser it will show pop up mesage beacuse it is browser but in software we cant use this
+//ecma script for java script
+console.log(3+3);
+console.log("neeraj ojha");//done
