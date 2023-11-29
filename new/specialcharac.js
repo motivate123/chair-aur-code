d21ba0e99d6a2794a10e6f77de8e30b4e67ccc80
motivate123 <151899380+motivate123@users.noreply.github.com>
2023-11-29 05:19:03 +0000

    special

---
+++ new/specialcharac.js
@@ -0,0 +1,6 @@
+console.log("Neeraj is good person \n also good in studies") //see here \n slash-is backslash which is used for new line 
+//for new thing read about special charcter of js on google
+let text=898
+let r ="neeraj"
+console.log(text,"\n",r)
+console.log(text, "\t", r)
