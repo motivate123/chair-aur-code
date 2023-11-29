f4b3707c7a9a7bd9823add1e26b1a4cd24d22f09
motivate123 <151899380+motivate123@users.noreply.github.com>
2023-11-29 06:34:23 +0000

    operator

---
+++ new/operators.js
@@ -0,0 +1,16 @@
+//operators in javascript
+/* 1. arithmetic operators 
+(*, +, -, **,/,%,++,--)*/
+/* 2. Assignment operators 
+(=,+=,**=,-=)*/
+/*3. comparison operators 
+(==,>,<,<=,>=,!=,===,!==) this last two are different let check it baaki is same as other language
+*/
+console.log(5==5)// here 5=5 is true;
+console.log(5=='5')/* here in this operator ==double in js its different this double one just doesnt check data type so just will throw true becuase in js 
+its doesnt compare data types it only comapres number*/
+console.log(5==='5')/* here this triple operator === in js it compares first data type if its not than throw false */
+console.log(5!=5)/* here this will throw false because its equal to 5 */
+console.log(5!='5')/* here it will throw also false but in this 5 is in string but it doesnt compare datattypes so will throw false*/
+console.log(5!=='5')// here it will compare data types and will throw true it compares datat types
+
