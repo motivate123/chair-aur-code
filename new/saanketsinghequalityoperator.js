4a2115922619146c2cff1dfc5ba04d0e50c578f5
motivate123 <151899380+motivate123@users.noreply.github.com>
2023-12-02 09:27:39 +0000

    don sanket singh

---
+++ new/saanketsinghequalityoperator.js
@@ -0,0 +1,3 @@
+/* == and === this both operator  compares values and types not only triple one (first one i have written plzz correct it when === this one compare if value is 
+    is not same return false , but other one will convert to number means coercion and than return which value to return but in === one if its is value is true it will return true)*/
+    console.log(5=="neeraj")// in this it will throw NAN beacuse it will be not possibel to convert to number
